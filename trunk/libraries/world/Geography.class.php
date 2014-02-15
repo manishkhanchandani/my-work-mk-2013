fd0ae53d97539b1066b60c7ffa64d51434f1d47e
manishkk74@gmail.com <manishkk74@gmail.com@fce71ee2-1617-2821-1fe7-b3c054611431>
2014-02-15 07:01:20 +0000

    nearby

--- trunk/libraries/world/Geography.class.php
+++ trunk/libraries/world/Geography.class.php
@@ -43,6 +43,22 @@ class world_Geography
 		}
 		return $return;
 	}
+	
+	
+	public function get_nearby_cities($lat, $lon, $radius=30, $order='distance', $limit=30)
+	{
+		global $connAdodb;
+		$sql = sprintf("select *, (ROUND(
+	DEGREES(ACOS(SIN(RADIANS(".GetSQLValueString($lat, 'double').")) * SIN(RADIANS(c.latitude)) + COS(RADIANS(".GetSQLValueString($lat, 'double').")) * COS(RADIANS(c.latitude)) * COS(RADIANS(".GetSQLValueString($lon, 'double')." -(c.longitude)))))*60*1.1515,2)) as distance from geo_cities as c WHERE (ROUND(
+	DEGREES(ACOS(SIN(RADIANS(".GetSQLValueString($lat, 'double').")) * SIN(RADIANS(c.latitude)) + COS(RADIANS(".GetSQLValueString($lat, 'double').")) * COS(RADIANS(c.latitude)) * COS(RADIANS(".GetSQLValueString($lon, 'double')." -(c.longitude)))))*60*1.1515,2)) <= ".GetSQLValueString($radius, 'int')." ORDER BY ".$order." LIMIT ".$limit);
+		$recordSet = $connAdodb->CacheExecute(_FUNC_TIME_DAY, $sql);
+		$return = array();
+		while (!$recordSet->EOF) {
+			$return[$recordSet->fields['cty_id']] = $recordSet->fields;
+			$recordSet->MoveNext();
+		}
+		return $return;
+	}
 }
 
 ?>
